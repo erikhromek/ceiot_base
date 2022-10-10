621db7836268c17fecbb8e19ce59f267c7cbd922
Erik Hromek <erikhromek96@gmail.com>
2022-10-10 23:00:15 +0000

    Fix usage of parameters for tools

--- tools/post_measurement_json.sh
+++ tools/post_measurement_json.sh
@@ -1,1 +1,1 @@
-curl -X POST http://localhost:8080/measurement.json -H "Content-Type: application/json" -d \'{"key":"$1", "id": "$2", "t": "$3", "h": "$4"}\'
+curl -X POST http://localhost:8080/measurement.json -H "Content-Type: application/json" -d '{"key":"'$1'","id":"'$2'","t":"'$3'","h":"'$4'"}'
