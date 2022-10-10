36767a7964416d71ed97739405697f31a3620233
Erik Hromek <erikhromek96@gmail.com>
2022-10-10 18:26:53 -0300

    Add config file

---
+++ esp32c3-bmp280/config.h
@@ -0,0 +1,37 @@
+#ifndef _config_
+
+#define API_IP        "192.168.0.100"
+#define API_PORT      "8080"
+#define API_IP_PORT   API_IP ":" API_PORT
+
+#define WIFI_SSID     "TeleCentro-8d26"
+#define WIFI_PASSWORD "lapicera"
+
+#define DEVICE_ID     "5"
+#define DEVICE_KEY    "esp-erik"
+
+
+// esp32-dht11 
+// #define ONE_WIRE_GPIO 17
+// #define USER_AGENT    "esp-idf/1.0 esp32"
+
+// esp32s2-dht11 
+// #define ONE_WIRE_GPIO 0
+// #define USER_AGENT    "esp-idf/1.0 esp32s2"
+
+// esp8266-dht11-arduino
+// #define ONE_WIRE_GPIO 2
+
+// esp32c3-bmp280 
+ #define SDA_GPIO      4
+ #define SCL_GPIO      5
+ #define USER_AGENT    "esp-idf/1.0 esp32c3"
+
+// esp32-bmp280 
+// #define SDA_GPIO     21
+// #define SCL_GPIO     22
+// #define USER_AGENT   "esp-idf/1.0 esp32c3""
+
+
+#define _config_
+#endif
