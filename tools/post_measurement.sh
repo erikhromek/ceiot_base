6344498059f88cc7c3c001f80278d563d03e1fae
Erik Hromek <erikhromek96@gmail.com>
2022-10-10 18:54:06 -0300

    Fix post measurement on tool

--- tools/post_measurement.sh
+++ tools/post_measurement.sh
@@ -1,1 +1,1 @@
-wget -O - --method=POST http://localhost:8080/measurement --body-data="key=$1id=$2&t=$3&h=$4"
+wget -O - --method=POST http://localhost:8080/measurement --body-data="key=$1&id=$2&t=$3&h=$4"
